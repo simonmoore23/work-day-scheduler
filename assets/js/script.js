39e77ec431d37947c79ca80ec6b6e015a8f9c208
Simon Moore <simonmoore23@gmail.com>
2023-11-01 15:12:33 +0000

    current hour highlighting correctly

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,12 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-var today = dayjs();
+
+$(document).ready(function () {
+var today = dayjs()
 $('#currentDay').text(today.format('D MMM  YYYY - h:mm A'));
+// var dateTime = $('#currentDay');
+
 
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
@@ -11,41 +15,49 @@ $(function () {
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
-  //
+  
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
-  //
+  
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
-  //
+  
   // TODO: Add code to display the current date in the header of the page.
 });
 
-
-// You'll need to use the [Day.js](https://day.js.org/en/) library to work with date and time. Be sure to read the documentation carefully and concentrate on using Day.js in the browser.
-
 // ## User Story
 
-// ```md
-// AS AN employee with a busy schedule
-// I WANT to add important events to a daily planner
-// SO THAT I can manage my time effectively
-// ```
 
-// ## Acceptance Criteria
-
-// ```md
-
-
-// WHEN I view the timeblocks for that day
-// THEN each timeblock is color coded to indicate whether it is in the past, present, or future
 // WHEN I click into a timeblock
 // THEN I can enter an event
 // WHEN I click the save button for that timeblock
 // THEN the text for that event is saved in local storage
 // WHEN I refresh the page
 // THEN the saved events persist
+
+var currentHour = dayjs().hour();
+
+function hourTracker() {
+  $('.time-block').each(function () {
+      var scheduleHour = $(this).attr("id").split("hour-")[1];
+      console.log("currentHour " + currentHour)
+      console.log("scheduleHour " + scheduleHour)
+
+      if (scheduleHour < currentHour) {
+          $(this).addClass("past");
+      }
+      else if (scheduleHour == currentHour) {
+          $(this).addClass("present");
+      }
+      else {
+          $(this).addClass("future");
+      }
+  })
+}
+
+hourTracker();
+});

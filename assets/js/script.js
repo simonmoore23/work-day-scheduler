23e4c0e17dc91b36b75a99e27604aadece848bc6
Simon Moore <simonmoore23@gmail.com>
2023-11-01 11:51:35 +0000

    date, time & schedule

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,9 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+var today = dayjs();
+$('#currentDay').text(today.format('D MMM  YYYY - h:mm A'));
+
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -36,11 +39,8 @@ $(function () {
 // ## Acceptance Criteria
 
 // ```md
-// GIVEN I am using a daily planner to create a schedule
-// WHEN I open the planner
-// THEN the current day is displayed at the top of the calendar
-// WHEN I scroll down
-// THEN I am presented with timeblocks for standard business hours of 9am&ndash;5pm
+
+
 // WHEN I view the timeblocks for that day
 // THEN each timeblock is color coded to indicate whether it is in the past, present, or future
 // WHEN I click into a timeblock
